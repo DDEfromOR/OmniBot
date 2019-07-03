971560dcabaf72056842bff5f2a533a8344e0211
Daniel Evans <dev@microsoft.com>
2019-07-03 12:49:22 -0700

    Create OmniBot

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild CoreBot.sln -p:DeployOnBuild=true -p:PublishProfile=devcleancorebot1-Web-Deploy.pubxml -p:Password=MK0MbrRsMt5CmnAPTCPb7gu9bk6vmhJsLs7lFaMfnpv07T7BffQZhxyJXYrt
+
